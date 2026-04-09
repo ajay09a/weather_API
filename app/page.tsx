6c29c2d3517f2d476599db6f5f31b4d1588a7d77
Ajay <ajaykaloniya@gmail.com>
2026-04-10 00:49:10 +0530

    fetch data and display

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,47 @@
 "use client";
+import { useState } from "react";
+import { getWeather } from "./lib/weather";
 
 export default function Home() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [err, setError] = useState("");
+
+  const handleSearch = async()=>{
+    try{
+      setLoading(true);
+      setError("");
+      const result = await getWeather(city);
+      console.log(result);
+      setData(result);
+    }
+    catch(err){
+      setError("city not found");
+    }
+    finally{
+      setLoading(false);
+    }
+  }
   return (
     <main className="main">
+      <h1 className="title">Weather App</h1>
+      <div className="searchbar">
+        <input className="input-box" placeholder="Enter City" value={city} onChange={(e)=>setCity(e.target.value)}></input>
+        <button className="search-button" onClick={handleSearch}>Search</button>
+      </div>
+      {loading && <p className="loading">loading...</p>}
+      {err && <p className="error">{err}</p>}
+
+      {data && (
+        <div className="result">
+          <h3>City: {data.address}</h3>
+          <p><b>Temperature: </b>{data.currentConditions.temp}°C</p>
+          <p><b>Wind Speed: </b>{data.currentConditions.windspeed}</p>
+          <p><b>Condition: </b>{data.currentConditions.conditions}</p>
+          <p><b>Description: </b>{data.description}</p>
+        </div>
+      )}
     </main>
   );
 }
