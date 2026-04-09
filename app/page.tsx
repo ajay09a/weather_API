bd0b6e369ac80070d813a1dc0460ccd9156d7cd3
Ajay <ajaykaloniya@gmail.com>
2026-04-10 01:29:14 +0530

    fix empty searches

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,11 @@ export default function Home() {
   const [err, setError] = useState("");
 
   const handleSearch = async()=>{
+    if(!city.trim()){
+      setData(null);
+      setError("Please enter a city");
+      return;
+    }
     try{
       setLoading(true);
       setError("");
@@ -27,36 +32,21 @@ export default function Home() {
     <main className="main">
       <h1 className="title">Weather App</h1>
       <div className="searchbar">
-        <input className="input-box" placeholder="Enter City" value={city} onChange={(e)=>setCity(e.target.value)}></input>
+        <input className="input-box" placeholder="Enter City" value={city} onKeyDown={(e)=>{if(e.key === "Enter"){handleSearch()}}} onChange={(e)=>setCity(e.target.value)}></input>
         <button className="search-button" onClick={handleSearch}>Search</button>
       </div>
       {loading && <p className="loading">loading...</p>}
       {err && <p className="error">{err}</p>}
 
-      {data && (
-        <div className="result">
-          <h3>City: {data.address}</h3>
-          <p><b>Temperature: </b>{data.currentConditions.temp}°C</p>
-          <p><b>Wind Speed: </b>{data.currentConditions.windspeed}</p>
-          <p><b>Condition: </b>{data.currentConditions.conditions}</p>
-          <p><b>Description: </b>{data.description}</p>
-
-          <div className="not-current">
-            <div className="yesterday">
-              <p><b>Temperature: </b>{data.days[0].temp}°C</p>
-              <p><b>Wind Speed: </b>{data.days[0].windspeed}</p>
-              <p><b>Condition: </b>{data.days[0].conditions}</p>
-              <p><b>Description: </b>{data.days[0].description}</p>
-            </div>
-            <div className="tomorrow">
-              <p><b>Temperature: </b>{data.days[2].temp}°C</p>
-              <p><b>Wind Speed: </b>{data.days[2].windspeed}</p>
-              <p><b>Condition: </b>{data.days[2].conditions}</p>
-              <p><b>Description: </b>{data.days[2].description}</p>
-            </div>
+      {data?.days?.map((day: any) => (
+          <div key={day.datetime} className="result">
+            <h3>{day.datetime}</h3>
+            <p><b>Temperature: </b>{day.temp}°C</p>
+            <p><b>Wind Speed: </b>{day.windspeed}</p>
+            <p><b>Condition: </b>{day.conditions}</p>
+            <p><b>Description: </b>{day.description}</p>
           </div>
-        </div>
-      )}
+        ))}
     </main>
   );
 }
